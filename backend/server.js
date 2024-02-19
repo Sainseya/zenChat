080ec68ede7960fd46501d7bfda751e4f567f4ff
sainseya <sainseya0x0@gmail.com>
2024-02-19 22:59:18 +0100

    fix : cors

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/dist")));
 
-  app.get("*", corsOptionsDelegate(), (req, res) =>
+  app.get("*", cors(),(req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
   );
 } else {
