9c2229735a84d222e4b287f09bd049f5d3cdf932
sainseya <sainseya0x0@gmail.com>
2024-02-19 23:06:06 +0100

    fix : cors

--- backend/server.js
+++ backend/server.js
@@ -11,23 +11,11 @@ const path = require("path");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 connectDB();
-const app = express() 
-dotenv.config()
+const app = express();
+dotenv.config();
 
-app.use(cors())
-
-// var allowlist = ['https://zenchat-61rp.onrender.com/', 'http://localhost:5000/']
-// var corsOptionsDelegate = function (req, callback) {
-//   var corsOptions;
-//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-//   } else {
-//     corsOptions = { origin: false } // disable CORS for this request
-//   }
-//   callback(null, corsOptions) // callback expects two parameters: error and options
-// }
-
-app.use(express.json()) // Accepte JSON Data
+app.use(cors());
+app.use(express.json()); // Accepte JSON Data
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
@@ -40,7 +28,7 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/dist")));
 
-  app.get("*", cors(),(req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
   );
 } else {
@@ -53,9 +41,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 5000; // Fallback to 5000 if PORT is not defined
 const server = app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+
 
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
